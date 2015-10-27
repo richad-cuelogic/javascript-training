d8a8e209af325c846a0e74c7eaef5508ef2cf546
Richa Dagar <richa.dagar@cuelogic.co.in>
2015-10-27 12:07:41 +0530

    POC- program 2 and 3 updated as per comments

--- JS/POC/Program 2/program.js
+++ JS/POC/Program 2/program.js
@@ -1,8 +1,8 @@
-/*Write a string replace method that will have the following signature:
-*/
-
 var str = 'My name is Pranay';
-var stringReplace = function(str1,str2){
-	return str.split(str1).join(str2);
+String.prototype.stringReplace  = function(str1,str2){
+	return this.split(str1).join(str2);
 };
-stringReplace("Pranay","Shamsher");
+
+str.stringReplace("Pranay","Shamsher");
+
+
--- JS/POC/Program 3/program.js
+++ JS/POC/Program 3/program.js
@@ -1,16 +1,17 @@
 var myArray = [1,2,3,4];
 Array.prototype.customPush  = function (element){
-	myArray[myArray.length] = element;
-	return myArray;
+	this[this.length] = element;
+	return this;
 };
 
 Array.prototype.customPop  =  function (){
-	return myArray.slice(0,-1);
+	return this.slice(0,-1);
 };
 
 myArray.customPush(15);
 myArray.customPush(17);
 myArray.customPop();
+
 
 
 

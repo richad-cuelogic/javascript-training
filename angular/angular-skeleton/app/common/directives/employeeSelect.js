1cc9f42d383b27697a221350aa8dca20093e5f6b
Richa Dagar <richa.dagar@cuelogic.co.in>
2015-11-10 19:44:29 +0530

    Day 4 practicals - 1,2,3

--- angular/angular-skeleton/app/common/directives/employeeSelect.js
+++ angular/angular-skeleton/app/common/directives/employeeSelect.js
@@ -1,24 +1,17 @@
 angular.module('employee.select', ['home.service'])
-.directive('employeeSelect',['$timeout','$location','homeService', function($timeout,$location,homeService) {
+.directive('employeeSelect',function() {
   return {
   	restrict: 'A',
-    templateUrl: 'app/home/views/employee-select.html',
     link : function($scope, $element, attr) {
     $element.on("click",function(){
-      $scope.isDisable = attr.isDisable;
-      $scope.inProcess = true;
-           homeService.updateEmployeeInfo(attr.employee).then(
-              function(response) {
-                 //$rootScope.employees = response;
-                 $timeout(function(){
-                  $scope.inProcess = false;
-                  $location.path('/home/');
-                 }, 
-                 3000);                          
-              }
-         );     	
+    	if($element.hasClass('bg-green')){
+    		$element.removeClass('bg-green');
+    	}else{
+    		$element.addClass('bg-green');
+    	}
+     	
      });
     }
 
   };
-}]);
+});

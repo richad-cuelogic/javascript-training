5e91f8d0ab1dd5537407db117d3abec3752e8dc5
Richa Dagar <richa.dagar@cuelogic.co.in>
2015-12-08 17:11:29 +0530

    Creating collection and fetching the info using mongo

--- node/myapp/index.js
+++ node/myapp/index.js
@@ -11,3 +11,49 @@ var server = app.listen(8081, function () {
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+
+///////////////////////////////////////////////////
+
+var mongodb = require('mongodb');
+
+var MongoClient = mongodb.MongoClient;
+
+var url = 'mongodb://localhost:27017/my_database_name';
+
+MongoClient.connect(url, function (err, db) {
+	//check db connection
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to', url);
+
+    var collection = db.collection('users');
+    // create collection in mongo
+    var user1 = {name: 'admin', age: 32, roles: ['admin', 'moderator', 'user']};
+    var user2 = {name: 'user', age: 22, roles: ['user']};
+    var user3 = {name: 'superadmin', age: 42, roles: ['super-admin', 'admin', 'moderator', 'user']};
+    //insert in db
+    collection.insert([user1, user2, user3], function (err, result) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Inserted documents into the "users" collection. The documents inserted with "_id" are:', result);
+      }
+     	//db.close();
+    });
+
+    //find from db
+    collection.find({name: 'user'}).toArray(function (err, result) {
+      if (err) {
+        console.log(err);
+      } else if (result.length) {
+        console.log('Found:', result);
+      } else {
+        console.log('No document(s) found with defined "find" criteria!');
+      }
+      db.close();
+    });
+     
+  }
+});
